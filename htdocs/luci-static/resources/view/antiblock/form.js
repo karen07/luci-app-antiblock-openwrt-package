c69d244d2da945f3abccd4c320902faca9e954a5
Khachatryan Karen <karen0734@gmail.com>
2025-03-10 21:17:21 +0300

    Add routes read

--- htdocs/luci-static/resources/view/antiblock/form.js
+++ htdocs/luci-static/resources/view/antiblock/form.js
@@ -11,24 +11,6 @@ return view.extend({
 
 		let o = s.option(form.Flag, 'enabled', _('Enabled'));
 
-		o = s.option(form.Value, 'url', _('URL'), _('Domains file URL, either File or URL or both'));
-		o.default = 'https://antifilter.download/list/domains.lst';
-		o.depends('enabled', '1');
-
-		o = s.option(form.Value, 'file', _('File'), _('Domains file path, either File or URL or both'));
-		o.depends('enabled', '1');
-
-		o = s.option(form.Value, 'DNS', _('DNS'), _('DNS address, required parameter'));
-		o.default = '1.1.1.1:53';
-		o.depends('enabled', '1');
-
-		o = s.option(form.Value, 'listen', _('Listen'), _('Listen address, required parameter'));
-		o.default = '192.168.1.1:5053';
-		o.depends('enabled', '1');
-
-		o = s.option(form.Value, 'gateway', _('Gateway'), _('Interface gateway, required parameter'));
-		o.depends('enabled', '1');
-
 		o = s.option(form.Value, 'output', _('Output'), _('Log or statistics output folder, optional parameter'));
 		o.depends('enabled', '1');
 
