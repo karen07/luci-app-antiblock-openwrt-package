21357ce4c491bedb59a69d2ed8245f2a07da2e6f
Khachatryan Karen <karen0734@gmail.com>
2026-05-24 13:19:41 +0300

    Fix

--- htdocs/luci-static/resources/view/antiblock/domains.js
+++ htdocs/luci-static/resources/view/antiblock/domains.js
@@ -10,16 +10,19 @@ let section_data;
 let domains_textarea;
 
 async function write_domains_handler() {
-    ui.showModal(null, [E('p', { class: 'spinning' }, _('Write domains'))]);
+    ui.showModal(null, [ E('p', {class : 'spinning'}, _('Write domains')) ]);
     const lines = domains_textarea.value.split(/\r?\n/).filter(Boolean);
     let write_data = '';
-    lines.forEach(function (element) { write_data += element + '\n' });
+    lines.forEach(function(element) { write_data += element + '\n' });
     const domains_path = section_routes.selectedOptions[0].label;
     try {
         await fs.write(domains_path, write_data);
-        await fs.exec('/etc/init.d/antiblock', ['restart']);
+        await fs.exec('/etc/init.d/antiblock', [ 'restart' ]);
     } catch (err) {
-        ui.addNotification(null, E('p', {}, _('Unable to write to domains file') + ' ' + domains_path + ' "' + err.message + '"'));
+        ui.addNotification(null,
+                           E('p', {},
+                             _('Unable to write to domains file') + ' ' +
+                                 domains_path + ' "' + err.message + '"'));
     }
     ui.hideModal();
     select_handler();
@@ -27,14 +30,23 @@ async function write_domains_handler() {
 
 function read_domains_handler(data) {
     const text_data = data.split(/\r?\n/).filter(Boolean);
-    const section_descr_div = E('div', { class: 'cbi-section-descr' }, _('Domain count in file:') + ' ' + text_data.length);
+    const section_descr_div =
+        E('div', {class : 'cbi-section-descr'},
+          _('Domain count in file:') + ' ' + text_data.length);
 
-    domains_textarea = E('textarea', { class: 'cbi-input-textarea' },);
+    domains_textarea = E(
+        'textarea',
+        {class : 'cbi-input-textarea'},
+    );
     domains_textarea.value = '';
-    text_data.forEach(function (element) { domains_textarea.value += element + '\n' });
+    text_data.forEach(function(
+        element) { domains_textarea.value += element + '\n' });
 
-    const btn_write_domains = E('button', { class: 'cbi-button cbi-button-apply', click: write_domains_handler }, _('Write domains'));
-    const div_for_btn = E('div', { style: 'padding-top: 20px' });
+    const btn_write_domains = E(
+        'button',
+        {class : 'cbi-button cbi-button-apply', click : write_domains_handler},
+        _('Write domains'));
+    const div_for_btn = E('div', {style : 'padding-top: 20px'});
     div_for_btn.appendChild(btn_write_domains);
 
     section_data.innerHTML = '';
@@ -46,41 +58,43 @@ function read_domains_handler(data) {
 function select_handler() {
     section_data.innerHTML = '';
     const domains_path = section_routes.selectedOptions[0].label;
-    fs.read_direct(domains_path).then(
-        read_domains_handler
-    ).catch(
-        function (err) {
+    fs.read_direct(domains_path)
+        .then(read_domains_handler)
+        .catch(function(err) {
             if (err.message == 'Failed to stat requested path') {
-                fs.exec('/bin/mkdir', ['/etc/antiblock']).then(
-                    fs.write(domains_path, '').then(
-                        read_domains_handler("")
-                    ).catch(
-                        function (err) {
-                            section_data.appendChild(E('p', {}, _('Unable to create domains file') + ' ' + domains_path + ' "' + err.message + '"'));
-                        }
-                    )
-                );
+                fs.exec('/bin/mkdir', [ '/etc/antiblock' ])
+                    .then(fs.write(domains_path, '')
+                              .then(read_domains_handler(""))
+                              .catch(function(err) {
+                                  section_data.appendChild(
+                                      E('p', {},
+                                        _('Unable to create domains file') +
+                                            ' ' + domains_path + ' "' +
+                                            err.message + '"'));
+                              }));
             } else {
-                section_data.appendChild(E('p', {}, _('Unable to read domains file') + ' ' + domains_path + ' "' + err.message + '"'));
+                section_data.appendChild(E('p', {},
+                                           _('Unable to read domains file') +
+                                               ' ' + domains_path + ' "' +
+                                               err.message + '"'));
             }
-        }
-    );
+        });
 }
 
 return view.extend({
-    handleSaveApply: null,
-    handleSave: null,
-    handleReset: null,
-    load: async function () {
-        return await uci.load('antiblock');
-    },
-    render: function () {
+    handleSaveApply : null,
+    handleSave : null,
+    handleReset : null,
+    load : async function() { return await uci.load('antiblock'); },
+    render : function() {
         const uci_routes = uci.sections('antiblock', 'route');
 
-        section_routes = E('select', { class: 'cbi-input-select', change: select_handler });
-        uci_routes.forEach(function (route) {
+        section_routes =
+            E('select', {class : 'cbi-input-select', change : select_handler});
+        uci_routes.forEach(function(route) {
             if (route.domains_path.substring(0, 4) != 'http') {
-                const routes_option = E('option', { value: route.domains_path }, route.domains_path);
+                const routes_option = E('option', {value : route.domains_path},
+                                        route.domains_path);
                 section_routes.appendChild(routes_option);
             }
         });
@@ -89,18 +103,21 @@ return view.extend({
         main_div.appendChild(E('h2', _('Domains')));
 
         if (section_routes.innerHTML != '') {
-            const routes_div = E('div', { class: 'cbi-section' });
-            routes_div.appendChild(E('div', { class: 'cbi-section-descr' }, _('Domains path:')));
+            const routes_div = E('div', {class : 'cbi-section'});
+            routes_div.appendChild(
+                E('div', {class : 'cbi-section-descr'}, _('Domains path:')));
             routes_div.appendChild(section_routes);
             main_div.appendChild(routes_div);
 
-            section_data = E('div', { class: 'cbi-section' });
+            section_data = E('div', {class : 'cbi-section'});
             main_div.appendChild(section_data);
 
             select_handler();
         } else {
-            const routes_div = E('div', { class: 'cbi-section' });
-            routes_div.appendChild(E('div', { class: 'cbi-section-descr' }, _('Path to file in "Domains path" is not set.')));
+            const routes_div = E('div', {class : 'cbi-section'});
+            routes_div.appendChild(
+                E('div', {class : 'cbi-section-descr'},
+                  _('Path to file in "Domains path" is not set.')));
             main_div.appendChild(routes_div);
         }
 
--- htdocs/luci-static/resources/view/antiblock/statistics.js
+++ htdocs/luci-static/resources/view/antiblock/statistics.js
@@ -8,19 +8,26 @@
 let main_config;
 
 return view.extend({
-    retrieveLog: async function () {
-        return fs.read_direct('/tmp/antiblock/stat.txt').then(function (logdata) {
-            const loglines = logdata.trim().split(/\n/).map(function (line) {
-                return line.replace(/^<\d+>/, '');
+    retrieveLog : async function() {
+        return fs.read_direct('/tmp/antiblock/stat.txt')
+            .then(function(logdata) {
+                const loglines = logdata.trim().split(/\n/).map(function(
+                    line) { return line.replace(/^<\d+>/, ''); });
+                return {
+                    value : loglines.join('\n'),
+                    rows : loglines.length + 1
+                };
+            })
+            .catch(function(err) {
+                ui.addNotification(null,
+                                   E('p', {},
+                                     _('Unable to load statistics data:') +
+                                         ' ' + err.message));
+                return '';
             });
-            return { value: loglines.join('\n'), rows: loglines.length + 1 };
-        }).catch(function (err) {
-            ui.addNotification(null, E('p', {}, _('Unable to load statistics data:') + ' ' + err.message));
-            return '';
-        });
     },
 
-    pollLog: async function () {
+    pollLog : async function() {
         const element = document.getElementById('syslog');
         if (element) {
             const log = await this.retrieveLog();
@@ -29,7 +36,7 @@ return view.extend({
         }
     },
 
-    load: async function () {
+    load : async function() {
         await uci.load('antiblock');
 
         main_config = uci.sections('antiblock', 'main');
@@ -42,11 +49,12 @@ return view.extend({
         return await this.retrieveLog();
     },
 
-    render: function (loglines) {
+    render : function(loglines) {
         const main_div = E([]);
         main_div.appendChild(E('h2', _('Statistics')));
-        const routes_div = E('div', { class: 'cbi-section' });
-        routes_div.appendChild(E('div', { class: 'cbi-section-descr' }, _('Statistics are not enabled.')));
+        const routes_div = E('div', {class : 'cbi-section'});
+        routes_div.appendChild(E('div', {class : 'cbi-section-descr'},
+                                 _('Statistics are not enabled.')));
         main_div.appendChild(routes_div);
 
         if (!main_config[0]?.stat || main_config[0]?.stat === '0') {
@@ -54,20 +62,19 @@ return view.extend({
         }
 
         return E([], [
-            E('h2', {}, [_('Statistics')]),
-            E('div', { 'id': 'content_syslog' }, [
-                E('textarea', {
-                    'id': 'syslog',
-                    'style': 'font-size:12px',
-                    'readonly': 'readonly',
-                    'wrap': 'off',
-                    'rows': loglines.rows
-                }, [loglines.value])
-            ])
+            E('h2', {}, [ _('Statistics') ]),
+            E('div', {'id' : 'content_syslog'}, [ E('textarea', {
+                  'id' : 'syslog',
+                  'style' : 'font-size:12px',
+                  'readonly' : 'readonly',
+                  'wrap' : 'off',
+                  'rows' : loglines.rows
+              },
+                                                    [ loglines.value ]) ])
         ]);
     },
 
-    handleSaveApply: null,
-    handleSave: null,
-    handleReset: null
+    handleSaveApply : null,
+    handleSave : null,
+    handleReset : null
 });
